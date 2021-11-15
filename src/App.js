d7c0f4a14174685b6ddcff01e358dc543da56c4d
venus <venus9023gold@mail.ru>
2021-11-15 09:25:21 -0800

    page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import Routers from "./router";
+// import NavBar from "./components/layout/navbar";
+// import Routers from "./router";
+// function App() {
+//   return (
+//     <>
+//       <NavBar/>
+//       <Routers/>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+import React from "react";
+import NavBar from './components/layout/navbar'
+import {BrowserRouter} from 'react-router-dom';
+import Routers from './router'
+
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <NavBar />
+      </BrowserRouter>
       <Routers/>
     </>
   );
